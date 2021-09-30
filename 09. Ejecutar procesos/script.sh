e560fdb01d9c41aa92640ad698560da0bdba12b4
Jerónimo Sánchez - socger <socger@gmail.com>
2021-09-30 12:39:19 +0200

    Ponemos comentarios explicativos

---
+++ 09. Ejecutar procesos/script.sh
@@ -0,0 +1,4 @@
+((suma = $1 + $2))
+
+echo "El inicio de un bash"
+echo "La suma es $suma"
--- 10. leer archivos/leer.js
+++ 10. leer archivos/leer.js
@@ -1,21 +1,29 @@
 var fs = require("fs");
 
-//var files = fs.readdirSync('./');
+// Esta es una lectura del directorio y no pasa hasta la siguiente línea (console.log) sin que termine de leer todo el directorio
+// var files = fs.readdirSync('./'); Rellena files con lo que hay en el directorio ./
+// console.log(files);
+
+// Ahora va a hacer una lectura asíncrona, deja el proceso trabajando y continua con lo que hubiera después
 fs.readdir('./', (error, files) => {
 
     if(error){
         throw error;
     }
-    console.log(files);
 
-    //var archivo = fs.readFileSync('./archivo.txt', 'UTF-8');
+    console.log(files); // Nos presenta el contenido del directorio actual ./
 
+
+    //var archivo = fs.readFileSync('./archivo.txt', 'UTF-8'); Lectura continua, no pasa a la siguiente línea hasta que no termina
+
+    // Lectura paralela del fichero ./archivo.txt ... console.log('Contenido del archivo...'); ... se ejecuta antes de que termine de abrir y leer el fichero
     fs.readFile('./archivo.txt','UTF-8', (error, archivo) => {
         if(error){
             throw error;
         }
         console.log(archivo);
     });
+
     console.log('Contenido del archivo...');
 });
 
